2ad5ee4e05017b5f6e0e592348a59343a4550d69
Andrew <kolbe1129@gmail.com>
2021-04-30 11:51:05 -0500

    Increased Text Size for Mobile

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function Home() {
         <nav className="absolute top-0 z-50 flex flex-wrap items-center justify-between w-full px-2 py-3 navbar-expand-lg">
           <div className="container flex flex-wrap items-center justify-between px-4 mx-auto">
             {/* <!-- Social Media Icons --> */}
-            <div className="flex text-3xl text-gray-100 md:ml-auto">
+            <div className="flex text-5xl text-gray-100 md:ml-auto">
               <a target='_blank' href='https://www.linkedin.com/in/andrewkolbe/' className='mx-2'><AiFillLinkedin /></a>
               <a target='_blank' href='https://github.com/andkolbe' className='mx-2'><AiFillGithub /></a>
               <a target='_blank' href='mailto:kolbe1129@gmail.com' className='mx-2'><AiOutlineMail /></a>
@@ -42,10 +42,10 @@ export default function Home() {
             <div className='flex flex-wrap items-center'>
               <div className='w-full px-4 ml-auto mr-auto text-center lg:w-6/12'>
                 <div data-aos='fade-in'>
-                  <h1 className='font-semibold text-gray-100 text-6xl lg:text-8xl'>
+                  <h1 className='font-semibold text-gray-100 text-7xl lg:text-8xl'>
                     Andrew Kolbe
                   </h1>
-                  <p className='mt-4 text-gray-100 text-2xl'>
+                  <p className='mt-4 text-gray-100 text-3xl lg:text-2xl'>
                     Web Developer | Birmingham, AL
               </p>
                 </div>
@@ -80,22 +80,22 @@ export default function Home() {
             <div className='flex flex-wrap'></div>
             <div className='flex flex-wrap items-center mt-32'>
               <div className='w-full px-4 ml-auto mr-auto md:w-5/12' data-aos='fade-right'>
-                <h2 className='my-6 md:my-2 text-4xl font-semibold leading-normal text-center md:text-left'>
+                <h2 className='my-6 md:my-2 text-5xl lg:text-4xl font-semibold leading-normal text-center md:text-left'>
                   About Me
           </h2>
-                <p className='mt-4 mb-4 text-lg leading-relaxed text-gray-900'>
+                <p className='mt-4 mb-4 text-xl lg:text-lg leading-relaxed text-gray-900'>
                   Hello! I'm Andrew, a pastry chef turned web developer from Texas who recently moved to Birmingham.
           </p>
-                <p className='mt-4 mb-4 text-lg leading-relaxed text-gray-900'>
+                <p className='mt-4 mb-4 text-xl lg:text-lg leading-relaxed text-gray-900'>
                   I specialize in full stack Javascript development and am interested in learning more about all things server side development.
           </p>
-                <p className='mt-0 mb-4 text-lg leading-relaxed text-gray-900'>
+                <p className='mt-0 mb-4 text-xl lg:text-lg leading-relaxed text-gray-900'>
                   I have years of experience working in high stress, team focused, and technically challenging environments. I love growing with and learning from a team and helping other motivated people reach their potential.
           </p>
-                <p className='mt-0 mb-4 text-lg leading-relaxed text-gray-900'>
+                <p className='mt-0 mb-4 text-xl lg:text-lg leading-relaxed text-gray-900'>
                   In my free time, I enjoy cooking and learning about new cuisines and cultures, listening to obscure metal, and spending time with my lazy dog.
           </p>
-                <p className='mt-0 mb-4 text-lg text-center md:text-left leading-relaxed font-bold text-gray-900'>
+                <p className='mt-0 mb-4 text-xl lg:text-lg text-center md:text-left leading-relaxed font-bold text-gray-900'>
                   View Resume
           </p>
               </div>
@@ -113,8 +113,8 @@ export default function Home() {
           <div className='container px-4 mx-auto mt-16' data-aos='fade-up'>
             <div className='flex flex-wrap items-center'>
               <div className='lg:w-3/5 md:px-4 px-2 ml-auto mr-auto md:pr-12'>
-                <h2 className='text-lg font-semibold mb-2 text-center'>Tech I Am Currently Using</h2>
-                <div className='flex justify-center text-xl mb-10'>
+                <h2 className='text-xl lg:text-lg font-semibold mb-2 text-center'>Tech I Am Currently Using</h2>
+                <div className='flex justify-center text-2xl lg:text-xl mb-10'>
                   <h6 className='mx-3'>Javascript</h6>
                   <h6 className='mx-3'>NodeJS</h6>
                   <h6 className='mx-3'>Express</h6>
@@ -124,8 +124,8 @@ export default function Home() {
                   <h6 className='mx-3'>Redis</h6>
                   <h6 className='mx-3'>React</h6>
                 </div>
-                <h2 className='text-lg font-semibold mb-2 text-center'>Tech I Am Interested In Learning</h2>
-                <div className='flex justify-center text-xl mb-12'>
+                <h2 className='text-xl lg:text-lg font-semibold mb-2 text-center'>Tech I Am Interested In Learning</h2>
+                <div className='flex justify-center text-2xl lg:text-xl mb-12'>
                   <h6 className='mx-3'>Go</h6>
                   <h6 className='mx-3'>Microservices</h6>
                   <h6 className='mx-3'>JS Testing</h6>
@@ -167,61 +167,28 @@ export default function Home() {
           <div className='container px-4 mx-auto md:mb-10'>
             <div className='flex flex-wrap items-center'>
               <div className='lg:w-3/5 md:px-4 px-2 ml-auto mr-auto md:pr-12'>
-                <h2 className='text-4xl font-semibold mb-20 text-center' data-aos='fade-left'>Projects</h2>
+                <h2 className='text-5xl lg:text-4xl font-semibold mb-20 text-center' data-aos='fade-left'>Projects</h2>
 
                 {/* Get The Dish */}
                 <div class='mb-40' data-aos='fade-left'>
-                  <h4 className='text-3xl text-gray-700 text-left'>Get The Dish</h4>
+                  <h4 className='text-4xl lg:text-3xl text-gray-700 text-left'>Get The Dish</h4>
                   <hr className='border-black mt-2 mb-4 w-1/4' />
-                  <h6 className='text-md'>A social networking service that allows users to create a profile, upload photos and restaurant information, and post about their favorite dishes they've eaten recently. Users can leave likes and comments on other users' dishes.</h6>
-                  <div className='text-xl mt-4'>Tech Used</div>
-                  <div className='flex justify-center mt-4 text-4xl text-black'>
+                  <h6 className='text-lg lg:text-md'>A social networking service that allows users to create a profile, upload photos and restaurant information, and post about their favorite dishes they've eaten recently. Users can leave likes and comments on other users' dishes.</h6>
+                  <div className='text-2xl lg:text-xl mt-4'>Tech Used</div>
+                  <div className='flex justify-center mt-4 text-5xl lg:text-4xl text-black'>
                     <SiTypescript className='mx-5' />
                     <FaNodeJs className='mx-5' />
                     <SiMysql className='mx-5' />
                     <FaReact className='mx-5' />
                     <SiBootstrap className='mx-5' />
                   </div>
-                  <div className='text-lg mt-10'>Full stack CRUD application from scratch</div>
-                  <div className='text-lg'>Images are hosted using AWS</div>
-                  <div className='text-lg'>Comments are updated real time with Socket.IO</div>
-                  <div className='text-lg'>Authentication via Passport JWT</div>
-                  <div className='text-lg'>Integrated a custom password reset workflow</div>
-                  <div className='text-lg'>3rd Party API integration with Stripe, Mailgun, and Yelp Fusion</div>
-                  <div className='text-lg'>Deployed with Heroku</div>
-                  <SRLWrapper>
-                    <div className='md:flex mt-6'>
-                      <img role='button' className=' md:w-1/3 w-2/3 mb-10 md:mr-5 object-contain' src="/Register-Page.png" />
-                      <img role='button' className=' md:w-1/3 w-2/3 mb-10 md:mr-5 h-auto' src="/New-Dish-Page-(3).png" />
-                      <img role='button' className=' md:w-1/3 w-2/3 object-contain' src="/Smores.png" />
-                    </div>
-                  </SRLWrapper>
-                  <div className='flex flex-col md:flex-row text-2xl my-12 font-bold justify-center'>
-                    <a target='_blank' href='https://get-the-dish.herokuapp.com/' className='mr-20'>View Site</a>
-                    <a target='_blank' href='https://github.com/andkolbe/get-the-dish' className='my-4 md:my-0 mr-20'>View Source Code</a>
-                    <a target='_blank' href='' className='md:my-0'>Design Document</a>
-                  </div>
-                </div>
-
-                {/* Chirper */}
-                <div class='mb-40' data-aos='fade-right'>
-                  <h4 className='text-3xl text-gray-700 text-left'>Chirper</h4>
-                  <hr className='border-black mt-2 mb-4 w-1/4' />
-                  <h6 className='text-md'>A social networking service that allows users to post and interact with messages. Registered users can post chirps, but unregistered users can only read them.</h6>
-                  <div className='text-xl mt-4'>Tech Used</div>
-                  <div className='flex justify-center mt-4 text-4xl text-black'>
-                    <FaNodeJs className='mx-5' />
-                    <SiMongodb className='mx-5' />
-                    <SiRedis className='mx-5' />
-                    <SiBootstrap className='mx-5' />
-                  </div>
-                  <div className='text-lg mt-10'>Rendered with the Handlebars templating engine</div>
-                  <div className='text-lg'>Mongoose ODM Library</div>
-                  <div className='text-lg'>Write about session storage</div>
-                  <div className='text-lg'>Authentication via Passport Google OAuth, Facebook and Twitter</div>
-                  <div className='text-lg'>Caching with Redis</div>
-                  <div className='text-lg'>Integration Testing with Jest and Puppeteer</div>
-                  <div className='text-lg'>Deployed with Heroku</div>
+                  <div className='text-xl lg:text-lg mt-10'>Full stack CRUD application from scratch</div>
+                  <div className='text-xl lg:text-lg'>Images are hosted using AWS</div>
+                  <div className='text-xl lg:text-lg'>Comments are updated real time with Socket.IO</div>
+                  <div className='text-xl lg:text-lg'>Authentication via Passport JWT</div>
+                  <div className='text-xl lg:text-lg'>Integrated a custom password reset workflow</div>
+                  <div className='text-xl lg:text-lg'>3rd Party API integration with Stripe, Mailgun, and Yelp Fusion</div>
+                  <div className='text-xl lg:text-lg'>Deployed with Heroku</div>
                   {/* <SRLWrapper>
                     <div className='md:flex mt-6'>
                       <img role='button' className=' md:w-1/3 w-2/3 mb-10 md:mr-5 object-contain' src="/Register-Page.png" />
@@ -229,7 +196,40 @@ export default function Home() {
                       <img role='button' className=' md:w-1/3 w-2/3 object-contain' src="/Smores.png" />
                     </div>
                   </SRLWrapper> */}
-                  <div className='flex flex-col md:flex-row text-2xl my-12 font-bold justify-center'>
+                  <div className='flex flex-col md:flex-row text-3xl lg:text-2xl my-12 font-bold justify-center'>
+                    <a target='_blank' href='https://get-the-dish.herokuapp.com/' className='mr-20'>View Site</a>
+                    <a target='_blank' href='https://github.com/andkolbe/get-the-dish' className='my-10 md:my-0 mr-20'>View Source Code</a>
+                    <a target='_blank' href='' className='md:my-0'>Design Document</a>
+                  </div>
+                </div>
+
+                {/* Chirper */}
+                <div class='mb-40' data-aos='fade-right'>
+                  <h4 className='text-4xl lg:text-3xl text-gray-700 text-left'>Chirper</h4>
+                  <hr className='border-black mt-2 mb-4 w-1/4' />
+                  <h6 className='text-lg lg:text-md'>A social networking service that allows users to post and interact with messages. Registered users can post chirps, but unregistered users can only read them.</h6>
+                  <div className='text-2xl lg:text-xl mt-4'>Tech Used</div>
+                  <div className='flex justify-center mt-4 text-5xl lg:text-4xl text-black'>
+                    <FaNodeJs className='mx-5' />
+                    <SiMongodb className='mx-5' />
+                    <SiRedis className='mx-5' />
+                    <SiBootstrap className='mx-5' />
+                  </div>
+                  <div className='text-xl lg:text-lg mt-10'>Rendered with the Handlebars templating engine</div>
+                  <div className='text-xl lg:text-lg'>Mongoose ODM Library</div>
+                  <div className='text-xl lg:text-lg'>Write about session storage</div>
+                  <div className='text-xl lg:text-lg'>Authentication via Passport Google OAuth, Facebook and Twitter</div>
+                  <div className='text-xl lg:text-lg'>Caching with Redis</div>
+                  <div className='text-xl lg:text-lg'>Integration Testing with Jest and Puppeteer</div>
+                  <div className='text-xl lg:text-lg'>Deployed with Heroku</div>
+                  {/* <SRLWrapper>
+                    <div className='md:flex mt-6'>
+                      <img role='button' className=' md:w-1/3 w-2/3 mb-10 md:mr-5 object-contain' src="/Register-Page.png" />
+                      <img role='button' className=' md:w-1/3 w-2/3 mb-10 md:mr-5 h-auto' src="/New-Dish-Page-(3).png" />
+                      <img role='button' className=' md:w-1/3 w-2/3 object-contain' src="/Smores.png" />
+                    </div>
+                  </SRLWrapper> */}
+                  <div className='flex flex-col md:flex-row text-3xl lg:text-2xl my-12 font-bold justify-center'>
                     <a target='_blank' href='https://get-the-dish.herokuapp.com/' className='mr-20'>View Site</a>
                     <a target='_blank' href='https://github.com/andkolbe/Chirper-Mongo' className='my-4 md:my-0 mr-20'>View Source Code</a>
                     <a target='_blank' href='' className='md:my-0'>Design Document</a>
@@ -238,18 +238,18 @@ export default function Home() {
 
                 {/* Strife */}
                 <div data-aos='fade-up'>
-                  <h4 className='text-3xl text-gray-700 text-left'>Strife</h4>
+                  <h4 className='text-4xl lg:text-3xl text-gray-700 text-left'>Strife</h4>
                   <hr className='border-black mt-2 mb-4 w-1/4' />
-                  <h6 className='text-md'>A chatting app that allows users to join rooms and communicate in real time with their friends.</h6>
-                  <div className='text-xl mt-4'>Tech Used</div>
-                  <div className='flex justify-center mt-4 text-4xl text-black'>
+                  <h6 className='text-lg lg:text-md'>A chatting app that allows users to join rooms and communicate in real time with their friends.</h6>
+                  <div className='text-2xl lg:text-xl mt-4'>Tech Used</div>
+                  <div className='flex justify-center mt-4 text-5xl lg:text-4xl text-black'>
                     <FaNodeJs className='mx-5' />
                     <SiRedis className='mx-5' />
                     <SiSocketDotIo className='mx-5' />
                   </div>
-                  <div className='text-lg mt-10'>Bidirectional Chatting with Socket.IO</div>
-                  <div className='text-lg'>Pub/Sub Integration with Redis</div>
-                  <div className='text-lg'>Deployed with Heroku</div>
+                  <div className='text-xl lg:text-lg mt-10'>Bidirectional Chatting with Socket.IO</div>
+                  <div className='text-xl lg:text-lg'>Pub/Sub Integration with Redis</div>
+                  <div className='text-xl lg:text-lg'>Deployed with Heroku</div>
                   {/* <SRLWrapper>
                     <div className='md:flex mt-6'>
                       <img role='button' className=' md:w-1/3 w-2/3 mb-10 md:mr-5 object-contain' src="/Register-Page.png" />
@@ -257,7 +257,7 @@ export default function Home() {
                       <img role='button' className=' md:w-1/3 w-2/3 object-contain' src="/Smores.png" />
                     </div>
                   </SRLWrapper> */}
-                  <div className='flex flex-col md:flex-row text-2xl my-12 font-bold justify-center'>
+                  <div className='flex flex-col md:flex-row text-3xl lg:text-2xl my-12 font-bold justify-center'>
                     <a target='_blank' href='https://get-the-dish.herokuapp.com/' className='mr-20'>View Site</a>
                     <a target='_blank' href='https://github.com/andkolbe/strife' className='my-4 md:my-0 mr-20'>View Source Code</a>
                     <a target='_blank' href='' className='md:my-0'>Design Document</a>
@@ -301,17 +301,17 @@ export default function Home() {
           <div className='container px-4 py-10 mx-auto lg:pt-14'>
             <div className='flex flex-wrap justify-center text-center' >
               <div className='w-full px-4 lg:w-7/12'>
-                <h2 className='text-4xl font-semibold text-gray-100'>
+                <h2 className='text-5xl lg:text-4xl font-semibold text-gray-100'>
                   Contact Me
           </h2>
-                <p className='mt-6 mb-4 text-lg leading-relaxed text-gray-500'>
+                <p className='mt-6 mb-4 text-xl lg:text-lg leading-relaxed text-gray-500'>
                   If you'd like to work together, contact me at
         <a className='font-bold emailText' href='mailto:kolbe1129@gmail.com' rel='noreferrer' target='_blank'> kolbe1129@gmail.com </a>
         or at any of the links below
           </p>
               </div>
             </div>
-            <div className='flex justify-center mt-6 text-6xl text-gray-100'>
+            <div className='flex justify-center mt-6 text-7xl lg:text-6xl text-gray-100'>
               <a target='_blank' href='https://www.linkedin.com/in/andrewkolbe/' className='mx-5'><AiFillLinkedin /></a>
               <a target='_blank' href='https://github.com/andkolbe' className='mx-5'><AiFillGithub /></a>
               <a target='_blank' href='mailto:kolbe1129@gmail.com' className='mx-5'><AiOutlineMail /></a>
@@ -321,7 +321,7 @@ export default function Home() {
         {/* Nav */}
 
 
-        <div className='text-center bg-black py-4'>
+        <div className='text-center bg-black py-4 text-xl lg:text-lg'>
           <p className='text-gray-100'>Designed with NextJS and Tailwind<a className='font-bold pl-10' target='_blank' href='https://github.com/andkolbe/Portfolio'> View Source Code </a></p>
         </div>
 
